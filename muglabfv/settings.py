c9df9bba03f6620ca420c721695b1880dd12ba17
safakoks <safakoksuzer@gmail.com>
2019-07-25 11:18:16 +0300

    Dockerfile updated

--- muglabfv/settings.py
+++ muglabfv/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
